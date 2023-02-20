9ab8bf95117b2d70f4f947db77603bff610a6d14
Tom <thomas.f.ward.89@gmail.com>
2023-02-20 17:17:03 +0000

    added prompt structure

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("utils/generateMarkdown");
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your name?',
-      name: 'name',
+      message: 'What is the title of your project?',
+      name: 'title',
     },
     {
       type: 'input',
-      message: 'What languages do you know?',
-      name: 'languages',
+      message: 'Write a description of the project',
+      name: 'description',
     },
     {
       type: 'input',
-      message: 'What is your preferred method of communication?',
-      name: 'comms',
+      message: 'Write installation instrucions',
+      name: 'installation',
     },
+    {
+      type: 'input',
+      message: 'Write usage instrucions',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Write contributing instructions',
+      name: 'contributing',
+    },
+    {
+      type: 'input',
+      message: 'What tests should be run?',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'Advise on how you respond to questions',
+      name: 'questions',
+    }
   ])
-
-
-
-
-
-
 
   // construction and file writing
   .then((answers) => {
