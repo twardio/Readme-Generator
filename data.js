edddebbefbb443313a0e3e2ce3f96fc6a75f2fb6
Tom <thomas.f.ward.89@gmail.com>
2023-02-20 18:38:07 +0000

    file writing improved

---
+++ data.js
@@ -0,0 +1,3 @@
+Name: undefined
+Languages: undefined
+Preferred method of communication: undefined
--- index.js
+++ index.js
@@ -1,53 +1,54 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const generateMarkdown = require("utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is the title of your project?',
+      message: 'Write the title of your project: ',
       name: 'title',
     },
     {
       type: 'input',
-      message: 'Write a description of the project',
+      message: 'Write a description of the project: ',
       name: 'description',
     },
     {
       type: 'input',
-      message: 'Write installation instrucions',
+      message: 'Write installation instrucions: ',
       name: 'installation',
     },
     {
       type: 'input',
-      message: 'Write usage instrucions',
+      message: 'Write usage instrucions: ',
       name: 'usage',
     },
     {
       type: 'input',
-      message: 'Write contributing instructions',
+      message: 'Write contributing instructions: ',
       name: 'contributing',
     },
     {
       type: 'input',
-      message: 'What tests should be run?',
+      message: 'What tests should be run?: ',
       name: 'tests',
     },
     {
       type: 'input',
-      message: 'Advise on how you respond to questions',
+      message: 'Advise on how you respond to questions:',
       name: 'questions',
     }
   ])
 
   // construction and file writing
   .then((answers) => {
-    const data = `Name: ${answers.name}\nLanguages: ${answers.languages}\nPreferred method of communication: ${answers.comms}\n`;
+    const data = `##${answers.title}
+    \n# Description ${answers.description}`;
 
-    fs.appendFile('data.js', data, (err) => {
+    fs.writeFile('Readmeinfo.js', data, (err) => {
       if (err) throw err;
-      console.log('Data saved to file!');
+      console.log('Data saved to your README.md file!');
     });
   });
